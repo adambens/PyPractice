e7d7eefbf01594c98f4b96f6e641ebb178aaa20f
Adam Benson <adambens@umich.edu>
2017-10-20 14:51:15 -0400

    Task 1 Completed

--- p1.py
+++ p1.py
@@ -1,11 +1,15 @@
 import re
 
 
-def find_urls(x):
-	x = re.findall('(^https*:\S+\.\S\S+)', x)
-	print(x)
+def find_urls(s):
+	regex = r"https?:\/\/[A-Za-z0-9]{2,}(?:\.+[A-Za-z0-9]{2,})+"
+	answer = re.findall(regex, s)
+	print(answer)
+	#parsed_urls = re.findall("http[s]?:\/\/[A-Za-z0-9]+(?:\.[A-Za-z0-9]{2,})\S+", input_string)
+	#x = re.findall('(^https*:\S+\.\S\S+)', x)
+	#print(parsed_urls)
 
 
-find_urls('http://bbc.co.k.tnt.lll')
+find_urls('http://bbc.co.k')
 
 #x = re.findall('^http:|https:\S+\.\S{2,}', x)
